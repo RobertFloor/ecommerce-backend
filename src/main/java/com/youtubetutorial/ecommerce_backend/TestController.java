7f272428fcf9748d42e6968f4525adb434d6c9f5
Robert Floor <robert.floor@schiphol.nl>
2024-06-17 21:57:54 +0200

    added database configuration

---
+++ src/main/java/com/youtubetutorial/ecommerce_backend/TestController.java
@@ -0,0 +1,13 @@
+package com.youtubetutorial.ecommerce_backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Test";
+    }
+}
